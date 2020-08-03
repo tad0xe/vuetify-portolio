dd6a978b155372cd3995b5e5b9229b2bf36a9f63
tad0xe <63855463+tad0xe@users.noreply.github.com>
2020-08-03 18:44:57 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template >
+  
+  <v-app dark style="margin:auto; max-width:1100px;">
+   
+
+    <v-main>
+    
+
+      <HelloWorld/>
+    <slid/>
+    <key/>
+    <About/>
+    <Home/>
+    
+    </v-main>
+  </v-app>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import slid from './components/slid'
+import key from './components/key'
+import About from './views/About'
+import Home from './views/Home'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    slid,
+    key,
+    About,
+    Home,
+   
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
